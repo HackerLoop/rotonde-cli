ca4eb152637ba5af3d1426c9e4bd7eec8a6d35dd
Constantin Clauzel <stantin04@gmail.com>
2016-04-17 13:31:49 +0200

    fixed paths, added conf variables

--- install_rpi.sh
+++ install_rpi.sh
@@ -1,3 +1,7 @@
+HYPRIOT_VERSION="1.10.3-1_armhf"
+HYPRIOT_URL=https://downloads.hypriot.com/docker-hypriot_$HYPRIOT_VERSION.deb
+INSTALL_DIR=/usr/local/bin
+
 # fix usb bugs for rpi
 CMD_LINE_FIX=" dwc_otg.fiq_fsm_mask=0xF"
 if ! grep -q "$CMD_LINE_FIX" /boot/cmdline.txt; then
@@ -10,10 +14,19 @@ if ! grep -q "$ETC_HOST"; then
   sudo su -c "echo \"$ETC_HOST\" >> /etc/hosts"
 fi
 
+# overlay kernel module is required for docker
 MOD_ENABLE="overlay"
 if ! grep -q "$MOD_ENABLE"; then
   sudo su -c "echo \"$MOD_ENABLE\" >> /etc/modules"
-  echo "!!!! rebooting ... please re-run this script on reboot ... reboot in 5 seconds"
+  echo
+  echo "#################################"
+  echo
+  echo "rebooting ... please re-run this script on reboot"
+  echo
+  echo "                        -> rebooting in 5 seconds"
+  echo
+  echo "#################################"
+  echo
   sleep 5
   sudo reboot
 fi
@@ -21,14 +34,15 @@ fi
 if ! which -s docker; then
   # TODO check version
   # download and install docker
-  curl -O https://downloads.hypriot.com/docker-hypriot_1.10.3-1_armhf.deb && sudo dpkg -i docker-hypriot_1.10.3-1_armhf.deb
+  curl -O $HYPRIOT_URL && sudo dpkg -i docker-hypriot_$HYPRIOT_VERSION.deb
 fi
 
 # start docker on boot
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo curl -o /usr/loca/bin/rotonde 
+sudo curl -o $INSTALL_DIR/rotonde https://raw.githubusercontent.com/HackerLoop/rotonde-cli/master/rotonde
+sudo chmod +x $INSTALL_DIR/rotonde
 
 # start rotonde modules, automatically restarts modules on failure (and on boot)
 sudo docker run --privileged --restart=always -d --name rotonde --net=host hackerloop/rotonde-rpi
